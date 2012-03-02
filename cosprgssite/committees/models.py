2445376a1bd310b0be6aff6a04d7f7e079bed076
Jeremy Nelson <jermnelson@gmail.com>
2012-03-02 10:45:31 -0700

    Adding support for reStructured Text in website

--- cosprgssite/committees/models.py
+++ cosprgssite/committees/models.py
@@ -1,3 +1,23 @@
+"""
+ :mod:models Django supporting models for Committee
+"""
+__author__ = 'Jeremy Nelson'
+
+
 from django.db import models
 
-# Create your models here.
+
+class CommitteeReportAuthor(models.Model):
+    user = User()
+
+class CommitteeReports(models.Model):
+    authors = models.ManyToManyKey(CommitteeAuthor)
+    ingested_date = models.DateTimeField(auto_now=True,
+                                         auto_now_add=True)
+    rstFileLocation = models.CharField(max_length=255)
+    report_date = models.DateTimeField()
+    
+
+
+
+
