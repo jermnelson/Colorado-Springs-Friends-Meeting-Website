edc5befeed70efe409cd822a683ee2b957817294
Jeremy Nelson <jermnelson@gmail.com>
2012-03-02 21:01:00 +0000

    Working on reStructure support for Committee Works

--- cosprgssite/committees/views.py
+++ cosprgssite/committees/views.py
@@ -1,1 +1,24 @@
-# Create your views here.
+"""
+ :mod:views Colorado Springs Friends Meeting Committee View
+"""
+__author__ = "Jeremy Nelson"
+from django.contrib.auth import authenticate
+from django.views.generic.simple import direct_to_template
+
+def display_report(request,
+                   year,
+                   month,
+                   day,
+                   report_name):
+     """
+     Function displays a reStructured Committee report
+
+     :param year: YYYY four year digit string
+     :param month: MM 01-12 digit 
+     :param day: DD 01-31 digit
+     :param report_name: Name of rst report (filename)
+     :rtype: Generated HTML
+     """
+     return direct_to_template(request,
+                               'committees/report.html',
+                               {})
