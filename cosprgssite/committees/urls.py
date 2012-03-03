ceeae4b4a8c2dc1c5a7140bcc2e97da3091d0ab1
Jeremy Nelson <jermnelson@gmail.com>
2012-03-02 17:22:37 -0700

    Continued work on Committees app

--- cosprgssite/committees/urls.py
+++ cosprgssite/committees/urls.py
@@ -1,16 +1,16 @@
 """
- mod:`url` Quaker Testimonies URL routing
+ mod:`url` Quaker Committee URL routing
 """
 __author__ = 'Jeremy Nelson'
 import testimonies.views
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('testimonies.views',
-    url(r"^$","default",name='Testimonies Home'),
-    url(r'community$','community'),
-    url(r'equality$','equality'),
-    url(r'integrity$','integrity'),
-    url(r'peace$','peace'),
-    url(r'simplicity$','simplicity'),
-    url(r'truth$','truth'),
+urlpatterns = patterns('committees.views',
+    url(r"^$","default",name='Committee Home'),
+    url(r'^/(\d{4})/(\d{2}/{\d{2}/$','get_report'),
+##    url(r'equality$','equality'),
+##    url(r'integrity$','integrity'),
+##    url(r'peace$','peace'),
+##    url(r'simplicity$','simplicity'),
+##    url(r'truth$','truth'),
 )
--- cosprgssite/committees/views.py
+++ cosprgssite/committees/views.py
@@ -2,8 +2,37 @@
  :mod:views Colorado Springs Friends Meeting Committee View
 """
 __author__ = "Jeremy Nelson"
+import os,settings
 from django.contrib.auth import authenticate
 from django.views.generic.simple import direct_to_template
+
+
+def get_report(rst_path):
+     """
+     Returns the reStructured text
+
+     :param rst_path: Path including file name of committee rst
+     """
+     try:
+          rst_file = open(rst_path,'rb')
+          rst_contents = rst_file.read()
+          rst_file.close()
+     except:
+          rst_file.close()
+     return rst_contents
+     
+
+def default(request):
+     """
+     Displays for committees of Meeting and Related organizations
+     """
+     if request.user.is_authenticated():
+          user = request.user
+     else:
+          user = None
+     return direct_to_template(request,
+                               'committees/index.html',
+                               {'user':user})
 
 def display_report(request,
                    year,
@@ -19,6 +48,21 @@ def display_report(request,
      :param report_name: Name of rst report (filename)
      :rtype: Generated HTML
      """
+     if request.user.is_authenticated():
+          user = request.user
+     else:
+          user = None
+     report_dir = os.path.join(settings.PROJECTBASE_DIR,
+                               year,
+                               month,
+                               day)
+     report_path = "%/%s" % (report_dir,report_name)
+     if os.path.exists(report_path):
+          # Should check datastore to see report protect,
+          report_rst = get_report(report_path)
+     else:
+          report_rst = '''Report %s does not exist''' % report_name
      return direct_to_template(request,
                                'committees/report.html',
-                               {})
+                               {'user':user,
+                                'report_rst':report_rst})
