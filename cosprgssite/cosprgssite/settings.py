0e8e1ff545bc50086e6f21e1ad7457dcfceb9933
Jeremy Nelson <jermnelson@gmail.com>
2013-10-21 22:19:20 -0600

    Refactoring continuing with Bootstrap 3 and Django 1.5.4

--- cosprgssite/cosprgssite/settings.py
+++ cosprgssite/cosprgssite/settings.py
@@ -71,8 +71,10 @@ STATIC_ROOT = ''
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
+
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'assets'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
